16ab310ef3376ae5730a31a29bfea2994d5a9fb8
Joel Mbiye <joembiye@gmail.com>
2022-10-01 13:07:26 +0100

    updated frontend with load balancer address

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a95df522872e146d6b30c70f1e5149f2-181810176.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
